c5dbe5df3ff74ff1b8063911937040237005cb59
Bhavesh Patil <patilbhavesh991209@gmail.com>
2023-12-27 21:30:04 +0530

    chore: chnage timeframe for operating hours
    - add model field to track product whether its active or not
    - change timeframe for all the operating hours
    - refactor templates

--- items/models/item.py
+++ items/models/item.py
@@ -5,19 +5,20 @@ from django.urls import reverse
 from items.models import Section, Category, OperatingHour
 
 
-class Item(models.Model):
+class Item(BaseModel):
     name = models.CharField(max_length=255, null=False)
     desc = models.TextField()
     section = models.ForeignKey(Section, on_delete=models.DO_NOTHING)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
-    operation_hour = models.ForeignKey(OperatingHour, on_delete=models.DO_NOTHING)
+    operation_hour = models.ManyToManyField(OperatingHour)
     base_price = models.FloatField()
-    tax = models.FloatField(default=18, editable=False)
+    tax = models.FloatField(default=18)
+    is_active = models.BooleanField(_("active"), default=True)
 
     @property
     def selling_price(self):
         # Your logic for calculating selling price
-        return round(self.base_price * 1.18, 1)
+        return round(self.base_price * (self.tax * 0.01 + 1), 1)
 
     def __str__(self):
         return self.name
