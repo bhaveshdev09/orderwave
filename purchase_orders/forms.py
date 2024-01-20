8b04d69a10bbed3caf6864dc911d8551819bd0eb
Bhavesh Patil <45851391+bhaveshdev09@users.noreply.github.com>
2024-01-20 22:51:22 +0530

    feat: upgrade functionality for purchase orders (#47)
    * tracking quantity of each material in purchase order
    
    Co-authored-by: bhaveshdev09 <patilbhavesh991209@gmail.com>

--- purchase_orders/forms.py
+++ purchase_orders/forms.py
@@ -1,30 +1,87 @@
 from django import forms
-from purchase_orders.models import PurchaseOrder
+from purchase_orders.models import PurchaseOrder, PurchaseOrderItem
 from materials.models import Material
+from vendors.models import Vendor
 from django.db.models import Sum
+from django.forms import inlineformset_factory
+
+
+class PurchaseOrderItemForm(forms.ModelForm):
+    quantity = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={
+                "class": "form-control item-qty",
+                "min": "1",
+                "value": "1",
+                "required": True,
+            }
+        ),
+        min_value=1,
+    )
+
+    class Meta:
+        model = PurchaseOrderItem
+        fields = ["material", "quantity"]
+        widgets = {
+            "material": forms.Select(
+                attrs={"class": "form-select item-select", "required": True}
+            ),
+        }
+
+
+PurchaseOrderItemFormSet = inlineformset_factory(
+    PurchaseOrder,
+    PurchaseOrderItem,
+    form=PurchaseOrderItemForm,
+    min_num=1,
+    can_delete=True,
+    extra=0,
+)
 
 
 class PurchaseOrderForm(forms.ModelForm):
     class Meta:
         model = PurchaseOrder
-        fields = ["vendor", "material", "status"]
+        fields = ["vendor", "status"]
         widgets = {
-            "vendor": forms.Select(attrs={"class": "form-control"}),
-            "material": forms.SelectMultiple(attrs={"class": "form-control"}),
-            "status": forms.Select(attrs={"class": "form-control"}),
+            "vendor": forms.Select(attrs={"class": "form-select"}),
+            "status": forms.Select(attrs={"class": "form-select"}),
         }
 
+    # Include fields for customer details
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.purchase_orderitem_formset = PurchaseOrderItemFormSet(
+            prefix="purchase_order_items"
+        )
+
+        try:
+            if self.instance is not None:
+                self.purchase_orderitem_formset = PurchaseOrderItemFormSet(
+                    instance=self.instance, prefix="purchase_order_items"
+                )
+
+        except Exception as e:
+            pass
+
+    def is_valid(self):
+        return True
+
     def save(self, commit=True):
-        instance = super(PurchaseOrderForm, self).save(commit=False)
+        self.cleaned_data = self.data
 
-        # Calculate the total price for the selected materials
-        total_price = Material.objects.filter(
-            id__in=self.cleaned_data["material"]
-        ).aggregate(total_price=Sum("price"))["total_price"]
-        instance.total = total_price if total_price else 0
+        if not self.instance:
+            self.instance = PurchaseOrder.objects.create(self.cleaned_data)
+        else:
+            self.instance.status = self.cleaned_data["status"]
+            self.instance.vendor = Vendor.objects.get(id=self.cleaned_data["vendor"])
+            self.instance.save()
 
-        if commit:
-            instance.save()
-            self.save_m2m()
+        self.purchase_orderitem_formset = PurchaseOrderItemFormSet(
+            self.cleaned_data, instance=self.instance, prefix="purchase_order_items"
+        )
 
-        return instance
+        if self.purchase_orderitem_formset.is_valid():
+            self.purchase_orderitem_formset.save()
+
+        return self.instance
--- purchase_orders/views.py
+++ purchase_orders/views.py
@@ -3,8 +3,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import PurchaseOrder
-from .forms import PurchaseOrderForm
+from purchase_orders.models import PurchaseOrder, Material
+from purchase_orders.forms import PurchaseOrderForm
 
 
 class PurchaseOrderListView(ListView):
@@ -19,12 +19,28 @@ class PurchaseOrderCreateView(CreateView):
     template_name = "purchase_orders/purchase_order_form.html"
     success_url = reverse_lazy("purchase_orders:purchase-order-list")
 
+    def get_context_data(self, **kwargs: Any):
+        self.context = super().get_context_data(**kwargs)
+        self.context["items"] = list(
+            Material.objects.only("id", "name", "price").values("id", "name", "price")
+        )
+        print(self.context)
+        return self.context
+
 
 class PurchaseOrderUpdateView(UpdateView):
     model = PurchaseOrder
     form_class = PurchaseOrderForm
     template_name = "purchase_orders/purchase_order_form.html"
     success_url = reverse_lazy("purchase_orders:purchase-order-list")
+
+    def get_context_data(self, **kwargs: Any):
+        self.context = super().get_context_data(**kwargs)
+        self.context["items"] = list(
+            Material.objects.only("id", "name", "price").values("id", "name", "price")
+        )
+        print(self.context)
+        return self.context
 
 
 class PurchaseOrderDeleteView(DeleteView):
