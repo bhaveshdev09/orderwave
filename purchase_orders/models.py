790429cb987d9ce0ad81884af991f3924d60ba13
bhaveshdev09 <patilbhavesh991209@gmail.com>
2024-01-20 22:49:29 +0530

    feat: upgrade functionality for purchase orders
    * tracking quantity of each material in purchase order

--- purchase_orders/models.py
+++ purchase_orders/models.py
@@ -13,7 +13,6 @@ class PurchaseOrder(BaseModel):
 
     po_no = models.CharField(max_length=20, editable=False)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    material = models.ManyToManyField(Material)
     total = models.FloatField(default=0, editable=False)
     order_date = models.DateTimeField(default=timezone.now)
     status = models.CharField(
@@ -27,7 +26,41 @@ class PurchaseOrder(BaseModel):
     def purchase_order_id(self):
         return f"PO-{self.order_date.strftime('%d%m%Y')}-{self.pk}"
 
+    def get_total(self):
+        return (
+            self.materials.all()
+            .aggregate(total=models.Sum("total_price", default=0))
+            .get("total")
+        )
+
+    @property
+    def total_cost(self):
+        return self.get_total()
+
     def save(self, *args, **kwargs):
         new_po_id = self._meta.model.objects.count() + 1
         self.po_no = str(new_po_id)
         super().save(*args, **kwargs)
+
+
+class PurchaseOrderItem(BaseModel):
+    material = models.ForeignKey(
+        Material, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.FloatField(editable=False)
+    purchase_order = models.ForeignKey(
+        PurchaseOrder,
+        related_name="materials",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        # Calculate total price before saving
+        self.total_price = self.material.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Item: {self.material.name}, Quantity: {self.quantity}"
